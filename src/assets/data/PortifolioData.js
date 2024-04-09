a71e9ef40f407e29b3dcfcb478fc0e6f0f397476
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-04-09 04:45:33 -0700

    portifolio

--- src/assets/data/PortifolioData.js
+++ src/assets/data/PortifolioData.js
@@ -3,6 +3,9 @@ import project01 from '../images/project1.png';
 import project02 from '../images/project2.png';
 import project03 from '../images/project3.png';
 import project04 from '../images/project4.png';
+import project05 from '../images/project05.png';
+import project06 from '../images/project06.png';
+import project07 from '../images/project07.png';
 
 const portfolios = [
     {
@@ -40,7 +43,43 @@ const portfolios = [
         description: "This system is KTS Ltd portfolio. It shows all descriptions of that company. It allows a user to chat with them, subscribe to newsletters, talk to their pharmacist, contact them, visit them. It is modern.",
         technologies: ["Javascript", "Golang", "Tailwindcss", "Reactjs", "MongoDb"],
         siteUrl: "https://kts.fly.dev/",
+    },
+    {
+        id: "05",
+        imgUrl: project05,
+        category: "web design",
+        title: "E-commerce Dashboard",
+        description: "This system is Ecommerce-admin-dashboard .",
+        technologies: ["Javascript", "Golang", "Tailwindcss", "Reactjs", "MongoDb"],
+        siteUrl: "https://the-best-admin-dashboard.netlify.app/ecommerce",
+    },
+    {
+        id: "06",
+        imgUrl: project06,
+        category: "web design",
+        title: "E-commerce Dashboard",
+        description: "This system is Ecommerce-admin-dashboard .",
+        technologies: ["Javascript", "Golang", "Tailwindcss", "Reactjs", "MongoDb"],
+        siteUrl: "https://the-best-admin-dashboard.netlify.app/ecommerce",
+    },
+    {
+        id: "07",
+        imgUrl: project07,
+        category: "web design",
+        title: "E-commerce shopfy dashboard",
+        description: "This system is Ecommerce-admin-dashboard .",
+        technologies: ["Javascript", "Golang", "Tailwindcss", "Reactjs", "MongoDb"],
+        siteUrl: "https://unique-swan-92fb6a.netlify.app/",
     }
+    // {
+    //     id: "04",
+    //     imgUrl: project04,
+    //     category: "web design",
+    //     title: "KTS Portfolio",
+    //     description: "This system is KTS Ltd portfolio. It shows all descriptions of that company. It allows a user to chat with them, subscribe to newsletters, talk to their pharmacist, contact them, visit them. It is modern.",
+    //     technologies: ["Javascript", "Golang", "Tailwindcss", "Reactjs", "MongoDb"],
+    //     siteUrl: "https://kts.fly.dev/",
+    // }
 ];
 
 export default portfolios;
